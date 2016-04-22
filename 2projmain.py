9eda6f3fc058825b1b1c28f0952f1c9e26ccbaa7
Mariana Mioto <mariana.mioto@gmail.com>
2016-04-22 17:18:33 -0300

    changing the plot of images

--- 2projmain.py
+++ 2projmain.py
@@ -12,15 +12,16 @@ image = ds.pixel_array
 
 binary_global, binary_adaptive = segmentation(image)
 
-fig, axes = plt.subplots(nrows=3, figsize=(20, 24))
+fig, axes = plt.subplots(1, 3, figsize=(20, 4))
 ax0, ax1, ax2 = axes
 plt.gray()
+
 
 ax0.imshow(image)
 ax0.set_title('Image')
 
 ax1.imshow(binary_global)
-ax1.set_title('Li  thresholding')
+ax1.set_title('Global  thresholding')
 
 ax2.imshow(binary_adaptive)
 ax2.set_title('Adaptive thresholding')
