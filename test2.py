9c756de1e82bae5a60592c8abeff844f783bbed0
Mariana Mioto <mariana.mioto@gmail.com>
2016-05-06 11:59:06 -0300

    limpando codigo de testes

--- test2.py
+++ test2.py
@@ -1,39 +1,23 @@
 #!/usr/bin/env python
 """Esta funcao realiza um contorno simples da imagem"""
-
-
-import PIL
 import pydicom
-import skimage
-import numpy as np
-
-from skimage import measure, data, io, filters
-
+from skimage import measure
 from utils import display
 
-from scipy.ndimage import gaussian_filter
+# adicione seu filtro aqui:
 
-#adicione seu filtro aqui:
 
 def contours(ds):
     level = ds.pixel_array.mean()
-    #print(level)
+    # print(level)
     contours = measure.find_contours(ds.pixel_array, level)
-    #print(contours)
+    # print(contours)
     for contour in contours:
         for x, y in contour:
             ds.pixel_array[x][y] = ds.pixel_array.max()
 
-def filterSobel(ds):
-    image = ds.pixel_array  # or any NumPy array!
-    edges = filters.sobel(image)
-    io.imshow(edges)
-    io.show()
-
-
 
 if __name__ == "__main__":
-    ds=pydicom.read_file("/Users/Mariana/Desktop/CT-0015.dcm")
-    #contours(ds)
+    ds = pydicom.read_file("/Users/Mariana/Desktop/CT-0015.dcm")
+    contours(ds)
     display(ds)
-    #filterSobel(ds)
