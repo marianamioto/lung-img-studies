c96df0d10b8a148214dfa508053e9ce4a814bc6b
Mariana Mioto <mariana.mioto@gmail.com>
2016-05-06 15:35:16 -0300

    limpando testes e corrigindo identacao

--- test2_exames.py
+++ test2_exames.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
 
-import PIL
 import pydicom
 import skimage
 import numpy as np
 import os
-import pylab
 
-from skimage import measure, data, io, filters
+from skimage import measure, filters
 
 from utils import display
 
+
 def contours(ds):
-      level = ds.pixel_array.mean()
-      #print(level)
-      contours = measure.find_contours(ds.pixel_array, level)
-      #print(contours)
-      for contour in contours:
-          for x, y in contour:
-              ds.pixel_array[x][y] = ds.pixel_array.max()
+    level = ds.pixel_array.mean()
+    # print(level)
+    contours = measure.find_contours(ds.pixel_array, level)
+    # print(contours)
+    for contour in contours:
+        for x, y in contour:
+            ds.pixel_array[x][y] = ds.pixel_array.max()
 
 PathDicom = "/Users/Mariana/Desktop/ILDdatabase/ILD_DB_txtROIs/3"
 
@@ -26,10 +25,9 @@ lstFilesDCM = []
 for dirName, subdirList, fileList in os.walk(PathDicom):
     for filename in fileList:
         if ".dcm" in filename.lower():
-            lstFilesDCM.append(os.path.join(dirName,filename))
+            lstFilesDCM.append(os.path.join(dirName, filename))
 
 for fileDCM in lstFilesDCM:
-    #fileDCM = lstFilesDCM[i]
-    ds=pydicom.read_file(fileDCM)
+    ds = pydicom.read_file(fileDCM)
     contours(ds)
-    #display(ds)
+    display(ds)
