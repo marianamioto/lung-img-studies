22a08dfa88c0b36c74fe32b3f8b8fa9da047191a
Mariana Mioto <mariana.mioto@gmail.com>
2016-05-06 15:58:34 -0300

    limpando comentarios e testes

--- test4.py
+++ test4.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 
-import matplotlib.pyplot as plt
 import pydicom
-import numpy
 
+from skimage.filters import threshold_otsu, threshold_adaptive
 
-from skimage import data
-from skimage.filters import threshold_otsu, threshold_adaptive, threshold_li
-
-#ds = pydicom.read_file("/Users/Mariana/Desktop/IM-0001-0033-0001.dcm")
-#image = ds.pixel_array
 
 def segmentation(image):
     global_thresh = threshold_otsu(image)
@@ -20,20 +14,8 @@ def segmentation(image):
 
     return binary_global, binary_adaptive
 
-#fig, axes = plt.subplots(nrows=3, figsize=(7, 8))
-#ax0, ax1, ax2 = axes
-#plt.gray()
 
-#ax0.imshow(image)
-#ax0.set_title('Image')
-
-#ax1.imshow(binary_global)
-#ax1.set_title('Otsu')
-
-#ax2.imshow(binary_adaptive)
-#ax2.set_title('Adaptativo')
-
-#for ax in axes:
-#    ax.axis('off')
-
-#plt.show()
+if __name__ == "__main__":
+    ds = pydicom.read_file("/Users/Mariana/Desktop/CT-0015.dcm")
+    image = ds.pixel_array
+    segmentation(image)
