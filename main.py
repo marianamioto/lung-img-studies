4340074ff3d0faf26cabf4fa7ad780701cb4ff7d
marianamioto <mariana.mioto@gmail.com>
2018-05-01 17:35:10 -0300

    Plotting image with features labeled

--- main.py
+++ main.py
@@ -6,16 +6,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pydicom
 
+from scipy import ndimage as ndi
 from skimage import morphology
+from skimage.color import label2rgb
 from skimage.measure import label
 
 
-def plot_image(pixel_array, title=None):
+def single_gray_plot(pixel_array, title=None):
     fig, ax = plt.subplots(figsize=(4, 3))
     ax.imshow(pixel_array, cmap=plt.cm.gray, interpolation='nearest')
     ax.axis('off')
     if title:
         ax.set_title(title)
+    plt.show()
+
+
+def plot_results(original_image, cleaned_image, features_labeled_image):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6, 3), sharex=True,
+                                   sharey=True)
+    ax1.imshow(original_image, cmap=plt.cm.gray, interpolation='nearest')
+    ax1.contour(cleaned_image, [0.5], linewidths=1.2, colors='y')
+    ax1.axis('off')
+    ax1.set_adjustable('box-forced')
+    ax2.imshow(features_labeled_image, interpolation='nearest')
+    ax2.axis('off')
+    ax2.set_adjustable('box-forced')
+    margins = dict(hspace=0.01, wspace=0.01, top=1, bottom=0, left=0, right=1)
+    fig.subplots_adjust(**margins)
     plt.show()
 
 
@@ -41,6 +58,16 @@ def remove_small_holes(pixel_array):
     )
 
 
+def label_features(cleaned_image, original_image):
+    labeled_image, _ = ndi.label(cleaned_image)
+    return label2rgb(
+        labeled_image,
+        image=original_image,
+        bg_label=0,
+        bg_color=(1, 0, 0),
+    )
+
+
 def main():
     try:
         image_path = sys.argv[1]
@@ -51,10 +78,20 @@ def main():
     dicom_image = open_dicom_image(image_path)
 
     markers = extract_markers(dicom_image.pixel_array)
-    plot_image(markers, 'Markers')
+    single_gray_plot(markers, 'Markers')
 
     removed_small_holes = remove_small_holes(markers)
-    plot_image(removed_small_holes, 'Removed Small Holes')
+    single_gray_plot(removed_small_holes, 'Removed Small Holes')
+
+    features_labeled = label_features(removed_small_holes,
+                                      dicom_image.pixel_array)
+    single_gray_plot(features_labeled)
+
+    plot_results(
+        dicom_image.pixel_array,
+        removed_small_holes,
+        features_labeled,
+    )
 
 
 if __name__ == '__main__':
