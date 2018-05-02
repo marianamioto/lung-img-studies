3e910b4d230c4e98cc20e2be4d5cf0dc46dfecf4
marianamioto <mariana.mioto@gmail.com>
2018-05-01 22:57:01 -0300

    Reworked lung segmentation

--- main.py
+++ main.py
@@ -8,13 +8,12 @@ import pydicom
 
 from scipy import ndimage as ndi
 from skimage import morphology
-from skimage.color import label2rgb
-from skimage.feature import canny
 from skimage.filters import sobel
-from skimage.measure import label
+
+DEBUG = False
 
 
-def single_gray_plot(pixel_array, title=None):
+def plot(pixel_array, title=None):
     fig, ax = plt.subplots(figsize=(12, 9))
     ax.imshow(pixel_array, cmap=plt.cm.gray, interpolation='nearest')
     ax.axis('off')
@@ -23,14 +22,13 @@ def single_gray_plot(pixel_array, title=None):
     plt.show()
 
 
-def plot_results(original_image, cleaned_image, features_labeled_image):
+def plot_results(original_image, result_image):
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6, 3), sharex=True,
                                    sharey=True)
     ax1.imshow(original_image, cmap=plt.cm.gray, interpolation='nearest')
-    ax1.contour(cleaned_image, [0.5], linewidths=1.2, colors='y')
     ax1.axis('off')
     ax1.set_adjustable('box-forced')
-    ax2.imshow(features_labeled_image, interpolation='nearest')
+    ax2.imshow(result_image, interpolation='nearest')
     ax2.axis('off')
     ax2.set_adjustable('box-forced')
     margins = dict(hspace=0.01, wspace=0.01, top=1, bottom=0, left=0, right=1)
@@ -49,52 +47,44 @@ def extract_markers(pixel_array):
     markers = np.zeros_like(pixel_array)
     markers[pixel_array < 700] = 1  # TODO: move to const
     markers[pixel_array > 800] = 2  # TODO: move to const
-    # markers[pixel_array > 2000] = 3  # TODO: move to const
     return markers
 
 
-def remove_small_holes(pixel_array):
-    #fill_lungs = label(
-    #    pixel_array,
-    #    return_num=False,
-    #)
-    #single_gray_plot(fill_lungs)
-    return morphology.remove_small_holes(
-        pixel_array,
-        500,  # TODO: move to const
-    )
+def segment_lungs(pixel_array):
 
+    img_gray_open = ndi.grey_opening(pixel_array, size=10, mode='wrap')
+    if DEBUG:
+        plot(img_gray_open, 'Gray Opening')
 
-def label_features(cleaned_image, original_image):
-    labeled_image, _ = ndi.label(cleaned_image)
-    return label2rgb(
-        labeled_image,
-        image=original_image,
-        bg_label=0,
-        bg_color=(1, 0, 0),
-    )
+    elavation_map = sobel(img_gray_open)
+    if DEBUG:
+        plot(elavation_map, 'Elevation Map')
 
+    markers = extract_markers(img_gray_open)
+    if DEBUG:
+        plot(markers, 'Markers')
 
-def apply_watershed(pixel_array):
-    elavation_map = sobel(pixel_array)
-    single_gray_plot(elavation_map, 'Elevation Map')
+    watersheded = morphology.watershed(elavation_map, markers)
+    if DEBUG:
+        plot(watersheded, 'Watershed')
 
-    # markers = remove_small_holes(pixel_array)
-    # single_gray_plot(markers, 'Markers no holes')
+    external_contour = ndi.binary_fill_holes(watersheded-1)
+    if DEBUG:
+        plot(external_contour, 'External Contour')
 
-    markers = extract_markers(pixel_array)
-    single_gray_plot(markers, 'Markers')
+    watersheded_no_contour = (watersheded - external_contour)
+    if DEBUG:
+        plot(watersheded_no_contour, 'Watershed (No Contour)')
 
-    segmentation = morphology.watershed(elavation_map, markers)
-    single_gray_plot(segmentation, 'watershed')
+    holes_filled = ndi.binary_fill_holes(watersheded_no_contour-1)
+    if DEBUG:
+        plot(holes_filled, 'Watershed (No Contour + Holes Filled)')
 
-    bin_filled = ndi.binary_fill_holes(segmentation - 1)
-    seg_minus_filled = (segmentation - bin_filled)
-    single_gray_plot(seg_minus_filled, 'seg - filled')
+    removed_noise = morphology.remove_small_objects(holes_filled, 300)
+    if DEBUG:
+        plot(removed_noise, 'Removed Noise')
 
-    single_gray_plot(morphology.convex_hull_object(seg_minus_filled - 1), 'convex_hull_image')
-    single_gray_plot(remove_small_holes(morphology.dilation(seg_minus_filled -1)), 'dilation')
-
+    return removed_noise
 
 
 def main():
@@ -105,16 +95,14 @@ def main():
         sys.exit(1)
 
     dicom_image = open_dicom_image(image_path)
-    single_gray_plot(dicom_image.pixel_array)
+    if DEBUG:
+        plot(dicom_image.pixel_array, 'Original')
 
-    segmented_image = apply_watershed(dicom_image.pixel_array)
-    single_gray_plot(segmented_image)
+    segmented_image = segment_lungs(dicom_image.pixel_array)
+    if DEBUG:
+        plot(segmented_image, 'Segmented Lungs')
 
-    # plot_results(
-    #     dicom_image.pixel_array,
-    #     removed_small_holes,
-    #     features_labeled,
-    # )
+    plot_results(dicom_image.pixel_array, segmented_image)
 
 
 if __name__ == '__main__':
